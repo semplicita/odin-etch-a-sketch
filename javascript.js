006defe4b980c5a95d6be6d08297409f3320de7a
semplicita <118811222+semplicita@users.noreply.github.com>
2022-11-24 22:02:40 +0200

    Add button for creating a new grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,30 @@
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 16*16; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
+function createGrid(width) {
+    for (let i = 0; i < width*width; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        
+        square.style.padding = 960/width/2 + "px"
     
-    square.style.padding = 960/16/2 + "px"
+        square.addEventListener('mouseover', () => {
+            square.classList.add('colored')
+        })
+        container.appendChild(square)
+    }
+}
 
-    square.addEventListener('mouseover', () => {
-        square.classList.add('colored')
-    })
-    container.appendChild(square)
+createGrid(16)
+
+const btn = document.querySelector('#btn')
+btn.onclick = () => {
+    let size = prompt("Set grid size: ")
+    if (size > 100) {
+        alert("Grid size can't be larger than 100!")
+    } else {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+        createGrid(size)
+    }
 }
